6c7ed1b673d2d0f1ebf58f881516eadcf9c3ec1d
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 13:12:57 +0300

    fixed frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
 document.body.innerHtml = '<h2>Initializing...</h2>';
 
 const ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
-ws.onmessage = (ev) => document.write(`${ev.data.user_name}: :${ev.data.emoji}:` + '<br/>');
+ws.onmessage = (ev) => {
+  console.log({ev})
+  document.write(`${ev.data.user_name}: :${ev.data.emoji}:` + '<br/>');
+};
