f34dfbb2a337e7805cef73ba641beffac05ae2bd
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 13:13:03 +0300

    build

--- public/main.js
+++ public/main.js
@@ -4,5 +4,6 @@ document.body.innerHtml = '<h2>Initializing...</h2>';
 
 var ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
 ws.onmessage = function (ev) {
-  return document.write(ev.data.user_name + ': :' + ev.data.emoji + ':' + '<br/>');
+  console.log({ ev: ev });
+  document.write(ev.data.user_name + ': :' + ev.data.emoji + ':' + '<br/>');
 };
