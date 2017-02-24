19f39b25d2be08260625488a8f81dd088f4d1655
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 15:45:05 +0300

    frontend fixed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,37 @@
-document.body.innerHtml = '<h2>Initializing...</h2>';
+const tag = (tagName, args, ...children) => children && children.length
+  ? `<${[tagName, args && args instanceof Array && args.map((v, k) => `${k}=${v}`)].filter(v => !!v).join()}>${children.join('')}</${tagName}>`
+  : `<${tagName}/>`;
 
-const ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
-ws.onmessage = (ev) => {
-  console.log({ev})
-  document.write(`${ev.data.user_name}: :${ev.data.emoji}:` + '<br/>');
+let elems = [
+  tag('h2', {}, 'Initializing...')
+];
+
+const render = () => {
+  const tree = tag('div', {}, ...elems)
+  console.log({elems, tree});
+  document.body.innerHTML = tree;
 };
+
+fetch('/likes/1')
+  .then(r => r.json())
+  .then(resp => {
+    const likes = resp.likes || [];
+    elems = [tag('p', {}, `Prevoius likes: ${likes.map(v => `:${v}:`).join(', ')}`)]
+    render();
+
+    const proto = location.protocol;
+    const wsProto = proto === 'https:' ? 'wss' : 'ws';
+    const ws = new WebSocket(`${wsProto}://${location.host}/`);
+
+    ws.onmessage = (ev) => {
+      let data = null;
+      try {
+        data = JSON.parse(ev.data);
+      } catch (e) {
+        data = {};
+      }
+
+      elems.push(tag('p', {}, `${data.user_name}: :${data.emoji}:`));
+      render();
+    };
+});
--- public/main.js
+++ public/main.js
@@ -1,9 +1,49 @@
 'use strict';
 
-document.body.innerHtml = '<h2>Initializing...</h2>';
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
-ws.onmessage = function (ev) {
-  console.log({ ev: ev });
-  document.write(ev.data.user_name + ': :' + ev.data.emoji + ':' + '<br/>');
+var tag = function tag(tagName, args) {
+  for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    children[_key - 2] = arguments[_key];
+  }
+
+  return children && children.length ? '<' + [tagName, args && args instanceof Array && args.map(function (v, k) {
+    return k + '=' + v;
+  })].filter(function (v) {
+    return !!v;
+  }).join() + '>' + children.join('') + '</' + tagName + '>' : '<' + tagName + '/>';
 };
+
+var elems = [tag('h2', {}, 'Initializing...')];
+
+var render = function render() {
+  var tree = tag.apply(undefined, ['div', {}].concat(_toConsumableArray(elems)));
+  console.log({ elems: elems, tree: tree });
+  document.body.innerHTML = tree;
+};
+
+fetch('/likes/1').then(function (r) {
+  return r.json();
+}).then(function (resp) {
+  var likes = resp.likes || [];
+  elems = [tag('p', {}, 'Prevoius likes: ' + likes.map(function (v) {
+    return ':' + v + ':';
+  }).join(', '))];
+  render();
+
+  var proto = location.protocol;
+  var wsProto = proto === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(wsProto + '://' + location.host + '/');
+
+  ws.onmessage = function (ev) {
+    var data = null;
+    try {
+      data = JSON.parse(ev.data);
+    } catch (e) {
+      data = {};
+    }
+
+    elems.push(tag('p', {}, data.user_name + ': :' + data.emoji + ':'));
+    render();
+  };
+});
