1271f6b5ba43f67884e5b23bd114e0da4b04c018
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 05:41:05 +0300

    fix frontend

--- public/main.js
+++ public/main.js
@@ -1,13 +1,4 @@
-'use strict';
-
-require('whatwg-fetch');
-
 document.body.innerHtml = '<h2>Initializing...</h2>';
-fetch('google.com').then(function (res) {
-  return document.write(res);
-});
 
 ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
-ws.onmessage = function (msg) {
-  return document.write(msg + '<br/>');
-};
+ws.onmessage = (msg) => document.write(msg + '<br/>');
