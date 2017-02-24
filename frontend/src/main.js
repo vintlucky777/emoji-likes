9b6d564cbb99d5136206b4b566b64cd3e7a58256
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 20:21:44 +0300

    emoji ui init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,23 +1,128 @@
-const tag = (tagName, args, ...children) => children && children.length
-  ? `<${[tagName, args && args instanceof Array && args.map((v, k) => `${k}=${v}`)].filter(v => !!v).join()}>${children.join('')}</${tagName}>`
-  : `<${tagName}/>`;
-
-let elems = [
-  tag('h2', {}, 'Initializing...')
-];
-
-const render = () => {
-  const tree = tag('div', {}, ...elems)
-  console.log({elems, tree});
-  document.body.innerHTML = tree;
+const uuid = () => {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 };
+
+const tag = (_tagName, _args, children) => {
+  const tagnclasses = _tagName.split('.');
+  let tagName = tagnclasses[0] || 'div';
+  let args = _args || {};
+  if (tagnclasses.length > 1) {
+    const [t, ...classes] = tagnclasses;
+    args.class = [args.class, ...classes].filter(v => !!v).join(' ');
+  }
+  const argsStr = _.map(args, (v, k) => `${k}="${v}"`).join(' ');
+
+  const tagWithAttrs = `${[tagName, argsStr].filter(v => !!v).join(' ')}`;
+
+  const resultElem = _.includes(['br', 'img', 'meta'], tagName)
+    ? `<${tagWithAttrs}/>`
+    : `<${tagWithAttrs}>${_.join(children, '')}</${tagName}>`;
+
+  return resultElem;
+};
+
+const initUser = () => {
+  let user_id = localStorage.getItem('user_id');
+  let user_name = localStorage.getItem('user_name');;
+
+  if (!user_id) {
+    user_id = uuid();
+    user_name = '';
+
+    localStorage.setItem('user_id', user_id);
+    localStorage.setItem('user_name', user_name);
+  }
+
+  return {id: user_id, name: user_name};
+}
+
+const user = initUser();
+
+const app = document.querySelector('#app');
+const render = (elems) => {
+  const tree = tag('.root', {}, elems);
+  const overlay = tag('.overlay');
+
+  if (app) {
+    app.innerHTML = tree + overlay;
+  }
+};
+
+const offset = {x: 0, y: 0};
+const onDrag = (rx, ry) => {
+  offset.x += rx;
+  offset.y += ry;
+  // console.log('onDrag', {rx, ry});
+};
+const onClick = (x, y) => {
+  console.log('onClick', {x, y});
+};
+
+let isAnimating = true;
+let emojis = null;
+const renderLoop = () => {
+  if (!emojis) {
+    emojis = document.querySelectorAll('.emoji');
+    console.log({emojis})
+  }
+  isAnimating && requestAnimationFrame(renderLoop);
+};
+
+const bindInputs = (DOMnode, {onClick, onDrag}) => {
+  let isPressed = false;
+  let pressStart = null;
+  let isDrag = false;
+
+  const onPressStart = (cx, cy) => {
+    isPressed = true;
+    pressStart = Date.now();
+  };
+  const onPressEnd = (cx, cy) => {
+    if (!isDrag && Date.now() - pressStart < 400) {
+      onClick(cx, cy);
+    }
+
+    isPressed = false;
+    pressStart = null;
+    isDrag = false;
+  };
+
+  const onPressDrag = (dx, dy) => {
+    if (!isPressed) {
+      return;
+    }
+
+    isDrag = true;
+    onDrag(100 * dx / app.clientWidth, 100 * dy / app.clientHeight);
+  };
+
+  DOMnode.onmousedown = (ev) => onPressStart(ev.offsetX, ev.offsetY);
+  DOMnode.onmousemove = (ev) => onPressDrag(ev.movementX, ev.movementY);
+  DOMnode.onmouseup = (ev) => onPressEnd(ev.offsetX, ev.offsetY);
+  DOMnode.ontouchstart = (ev) => onPressStart(ev.offsetX, ev.offsetY);
+  DOMnode.ontouchmove = (ev) => onPressDrag(ev.movementX, ev.movementY);
+  DOMnode.ontouchend = (ev) => onPressEnd(ev.offsetX, ev.offsetY);
+};
+
+bindInputs(app, {onClick, onDrag});
+
+const initApp = () => {
+  const mesh = _.flatten(_.map(_.range(-480, 481, 120), x => _.map(_.range(-480, 481, 120), y => [x, y])));
+  const elems = _.map(mesh, ([x, y]) => tag('.emoji', {style: `transform: translate3d(${x}%, ${y}%, 0)`}));
+
+  render(elems);
+}
 
 fetch('/likes/1')
   .then(r => r.json())
   .then(resp => {
     const likes = resp.likes || [];
-    elems = [tag('p', {}, `Prevoius likes: ${likes.map(v => `:${v}:`).join(', ')}`)]
-    render();
 
     const proto = location.protocol;
     const wsProto = proto === 'https:' ? 'wss' : 'ws';
@@ -31,7 +136,9 @@ fetch('/likes/1')
         data = {};
       }
 
-      elems.push(tag('p', {}, `${data.user_name}: :${data.emoji}:`));
-      render();
+      console.log(`${data.user_name}: :${data.emoji}:`);
     };
 });
+
+initApp();
+renderLoop();
--- public/main.js
+++ public/main.js
@@ -1,35 +1,165 @@
 'use strict';
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var tag = function tag(tagName, args) {
-  for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-    children[_key - 2] = arguments[_key];
-  }
+function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
 
-  return children && children.length ? '<' + [tagName, args && args instanceof Array && args.map(function (v, k) {
-    return k + '=' + v;
-  })].filter(function (v) {
-    return !!v;
-  }).join() + '>' + children.join('') + '</' + tagName + '>' : '<' + tagName + '/>';
+var uuid = function uuid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 };
 
-var elems = [tag('h2', {}, 'Initializing...')];
+var tag = function tag(_tagName, _args, children) {
+  var tagnclasses = _tagName.split('.');
+  var tagName = tagnclasses[0] || 'div';
+  var args = _args || {};
+  if (tagnclasses.length > 1) {
+    var _tagnclasses = _toArray(tagnclasses),
+        t = _tagnclasses[0],
+        classes = _tagnclasses.slice(1);
 
-var render = function render() {
-  var tree = tag.apply(undefined, ['div', {}].concat(_toConsumableArray(elems)));
-  console.log({ elems: elems, tree: tree });
-  document.body.innerHTML = tree;
+    args.class = [args.class].concat(_toConsumableArray(classes)).filter(function (v) {
+      return !!v;
+    }).join(' ');
+  }
+  var argsStr = _.map(args, function (v, k) {
+    return k + '="' + v + '"';
+  }).join(' ');
+
+  var tagWithAttrs = '' + [tagName, argsStr].filter(function (v) {
+    return !!v;
+  }).join(' ');
+
+  var resultElem = _.includes(['br', 'img', 'meta'], tagName) ? '<' + tagWithAttrs + '/>' : '<' + tagWithAttrs + '>' + _.join(children, '') + '</' + tagName + '>';
+
+  return resultElem;
+};
+
+var initUser = function initUser() {
+  var user_id = localStorage.getItem('user_id');
+  var user_name = localStorage.getItem('user_name');;
+
+  if (!user_id) {
+    user_id = uuid();
+    user_name = '';
+
+    localStorage.setItem('user_id', user_id);
+    localStorage.setItem('user_name', user_name);
+  }
+
+  return { id: user_id, name: user_name };
+};
+
+var user = initUser();
+
+var app = document.querySelector('#app');
+var render = function render(elems) {
+  var tree = tag('.root', {}, elems);
+  var overlay = tag('.overlay');
+
+  if (app) {
+    app.innerHTML = tree + overlay;
+  }
+};
+
+var offset = { x: 0, y: 0 };
+var onDrag = function onDrag(rx, ry) {
+  offset.x += rx;
+  offset.y += ry;
+  // console.log('onDrag', {rx, ry});
+};
+var onClick = function onClick(x, y) {
+  console.log('onClick', { x: x, y: y });
+};
+
+var isAnimating = true;
+var emojis = null;
+var renderLoop = function renderLoop() {
+  if (!emojis) {
+    emojis = document.querySelectorAll('.emoji');
+    console.log({ emojis: emojis });
+  }
+  isAnimating && requestAnimationFrame(renderLoop);
+};
+
+var bindInputs = function bindInputs(DOMnode, _ref) {
+  var onClick = _ref.onClick,
+      onDrag = _ref.onDrag;
+
+  var isPressed = false;
+  var pressStart = null;
+  var isDrag = false;
+
+  var onPressStart = function onPressStart(cx, cy) {
+    isPressed = true;
+    pressStart = Date.now();
+  };
+  var onPressEnd = function onPressEnd(cx, cy) {
+    if (!isDrag && Date.now() - pressStart < 400) {
+      onClick(cx, cy);
+    }
+
+    isPressed = false;
+    pressStart = null;
+    isDrag = false;
+  };
+
+  var onPressDrag = function onPressDrag(dx, dy) {
+    if (!isPressed) {
+      return;
+    }
+
+    isDrag = true;
+    onDrag(100 * dx / app.clientWidth, 100 * dy / app.clientHeight);
+  };
+
+  DOMnode.onmousedown = function (ev) {
+    return onPressStart(ev.offsetX, ev.offsetY);
+  };
+  DOMnode.onmousemove = function (ev) {
+    return onPressDrag(ev.movementX, ev.movementY);
+  };
+  DOMnode.onmouseup = function (ev) {
+    return onPressEnd(ev.offsetX, ev.offsetY);
+  };
+  DOMnode.ontouchstart = function (ev) {
+    return onPressStart(ev.offsetX, ev.offsetY);
+  };
+  DOMnode.ontouchmove = function (ev) {
+    return onPressDrag(ev.movementX, ev.movementY);
+  };
+  DOMnode.ontouchend = function (ev) {
+    return onPressEnd(ev.offsetX, ev.offsetY);
+  };
+};
+
+bindInputs(app, { onClick: onClick, onDrag: onDrag });
+
+var initApp = function initApp() {
+  var mesh = _.flatten(_.map(_.range(-480, 481, 120), function (x) {
+    return _.map(_.range(-480, 481, 120), function (y) {
+      return [x, y];
+    });
+  }));
+  var elems = _.map(mesh, function (_ref2) {
+    var _ref3 = _slicedToArray(_ref2, 2),
+        x = _ref3[0],
+        y = _ref3[1];
+
+    return tag('.emoji', { style: 'transform: translate3d(' + x + '%, ' + y + '%, 0)' });
+  });
+
+  render(elems);
 };
 
 fetch('/likes/1').then(function (r) {
   return r.json();
 }).then(function (resp) {
   var likes = resp.likes || [];
-  elems = [tag('p', {}, 'Prevoius likes: ' + likes.map(function (v) {
-    return ':' + v + ':';
-  }).join(', '))];
-  render();
 
   var proto = location.protocol;
   var wsProto = proto === 'https:' ? 'wss' : 'ws';
@@ -43,7 +173,9 @@ fetch('/likes/1').then(function (r) {
       data = {};
     }
 
-    elems.push(tag('p', {}, data.user_name + ': :' + data.emoji + ':'));
-    render();
+    console.log(data.user_name + ': :' + data.emoji + ':');
   };
 });
+
+initApp();
+renderLoop();
