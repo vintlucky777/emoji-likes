bb26b8599e67517d18652b67d7faadb3361ba1d8
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 13:03:09 +0300

    better frontend code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
 document.body.innerHtml = '<h2>Initializing...</h2>';
 
 const ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
-ws.onmessage = (msg) => document.write(msg + '<br/>');
+ws.onmessage = (ev) => document.write(`${ev.data.user_name}: :${ev.data.emoji}:` + '<br/>');
