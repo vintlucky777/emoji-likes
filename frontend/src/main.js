9e61fe708b951550286e7b4d4ae83919fce8d677
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-24 12:56:00 +0300

    fix frontend js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
 document.body.innerHtml = '<h2>Initializing...</h2>';
 
-ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
+const ws = new WebSocket('wss://clojure-likes.herokuapp.com/');
 ws.onmessage = (msg) => document.write(msg + '<br/>');
