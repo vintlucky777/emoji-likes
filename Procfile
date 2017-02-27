25e5ccf285cf4f76edd1e3e82a55003959832910
Victor Klochikhin <victor.klochikhin@truckerpath.com>
2017-02-27 15:37:40 +0300

    got rid of deploy.sh

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm boot-server
+web: npm install && npm run boot-server
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-npm run build-fe
-npm run build-server
-git stage --all && git commit -m 'build' || echo 'nothing to commit'
-git push heroku master
